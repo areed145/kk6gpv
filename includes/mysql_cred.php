08f7cebd77f6d64aff14df9d5131a293d88a64da
areed145 <areed145@gmail.com>
2018-01-30 12:57:51 -0800

    Update mysql_cred.php

--- includes/mysql_cred.php
+++ includes/mysql_cred.php
@@ -5,10 +5,11 @@ $databasename = "enjtvqmjpz";
 $databaseusername="enjtvqmjpz";
 $databasepassword = "8eCrExD8aX";
 
-$wxtable = "t_wx_raw_xml";
+$wxtable = "t_wx_raw";
 $wxtable_filter = "v_wx_filter";
-$wxtable_wind = "v_wx_wind";
-$wxtable_winddrange = "t_wx_windrose_drange";
+$wxtable_wind = "v_wx_wr";
+$wxtable_winddrange = "t_wx_wrdrange";
+$wxtable_windsrange = "t_wx_wrsrange";
 $aprstable = "t_aprs_raw";
 $awstable = "v_aws";
 $awstableraw = "t_aws_raw";
