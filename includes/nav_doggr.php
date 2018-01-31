bc848b79ffef2fd962a9bbc2868362a7c5a66a5a
areed145 <areed145@gmail.com>
2018-01-31 16:19:31 +0000

    Create nav_doggr.php

---
+++ includes/nav_doggr.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<?php $utc_now = gmdate("Y-m-d H:i:s");?>
+<html>
+<p align=center>
+<a href="doggr.php?opcode=E3700">Exxon</a> | 
+<a href="doggr.php?opcode=V1370">Vintage</a> | 
+<a href="doggr.php?opcode=C5640">Chevron</a> | 
+<a href="doggr.php?opcode=E0100">E&B</a>
+</p><hr></html>
