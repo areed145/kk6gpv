8388c3a18cfa1eb05649f0fe3ab354dd5170e126
areed145 <areed145@gmail.com>
2018-01-31 16:21:22 +0000

    Update nav_doggr.php

--- includes/nav_doggr.php
+++ includes/nav_doggr.php
@@ -5,7 +5,8 @@
 <a href="doggr.php?opcode=E3700">Exxon</a> | 
 <a href="doggr.php?opcode=V1370">Vintage</a> | 
 <a href="doggr.php?opcode=C5640">Chevron</a> | 
-<a href="doggr.php?opcode=E0100">E&B</a>
+<a href="doggr.php?opcode=E0100">E&ampB</a> |
+<a href="doggr.php?fieldcode=340">Kern River</a>
 </p>
 <hr>
 </html>
