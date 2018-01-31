0772e5b3c00d8a5ccbc887975050b5c6c1d06a2f
areed145 <areed145@gmail.com>
2018-01-30 20:58:43 -0800

    fix link

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,5 @@
 <!doctype html>
 <html>
-
 <hr>
-<p align=center>&copy; 2017 Adam Reeder <a href=http://www.kk6gpv.net/index.php'>http://www.kk6gpv.net</a></p>
-
+<p align=center>&copy; 2017 Adam Reeder <a href="http://www.kk6gpv.net/index.php">http://www.kk6gpv.net</a></p>
 </html>
